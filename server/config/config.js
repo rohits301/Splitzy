4ea85807f1acf7351e7779b4d6735e0573f6f6d0
Rohit Sachdeva <sachdevarohit5@gmail.com>
2025-06-30 23:26:34 +0530

    chore: moved to config.js for dynamic configs

---
+++ server/config/config.js
@@ -0,0 +1,21 @@
+require('dotenv').config(); // important so it can load .env vars
+
+module.exports = {
+  development: {
+    username: process.env.DB_USER || 'postgres',
+    password: process.env.DB_PASSWORD || null,
+    database: process.env.DB_NAME || 'splitzy_db_dev',
+    host: process.env.DB_HOST || '127.0.0.1',
+    port: process.env.DB_PORT || 5432,
+    dialect: 'postgres',
+    logging: console.log
+  },
+  production: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT || 5432,
+    dialect: 'postgres'
+  }
+};
--- server/models/index.js
+++ server/models/index.js
@@ -6,7 +6,7 @@ const Sequelize = require('sequelize');
 const process = require('process');
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.json')[env];
+const config = require(__dirname + '/../config/config.js')[env];
 const db = {};
 
 let sequelize;
@@ -39,5 +39,13 @@ Object.keys(db).forEach(modelName => {
 
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
+
+// Test connection
+db.sequelize.authenticate()
+  .then(() => console.log('Connection to PostgreSQL has been established successfully.'))
+  .catch(err => {
+    console.error('Unable to connect to the PostgreSQL database:', err);
+    process.exit(1);
+  });
 
 module.exports = db;
